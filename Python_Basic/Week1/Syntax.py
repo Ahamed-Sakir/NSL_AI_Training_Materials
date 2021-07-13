48238f5f72d431b00347c13729f620aa13459816
Ahamed-Sakir <87180676+Ahamed-Sakir@users.noreply.github.com>
2021-07-13 09:59:05 +0600

    Update Syntax.py

--- Python_Basic/Week1/Syntax.py
+++ Python_Basic/Week1/Syntax.py
@@ -1,9 +1,15 @@
 # Basic String syntax
-Name = "Sakir Ahamed"
-print(Name)
+name = "Sakir Ahamed"
+print(name)
 
 # We can use single quote also
-Name1 = 'Bangladesh is my country'
-print(Name1)
+name1 = 'Bangladesh is my country'
+print(name1)
 
-# 
+# If we use single quote in a sentence then use '\'
+single_quote = 'This is Anannyar\'s property'
+print(single_quote)
+
+# User input
+user_input = input('Give your name:')
+print('This is your name:', user_input)
