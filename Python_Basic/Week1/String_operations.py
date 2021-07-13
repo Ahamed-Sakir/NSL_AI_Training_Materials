b88e456173028bdb0fd62f44be0071652b2665a1
Ahamed-Sakir <87180676+Ahamed-Sakir@users.noreply.github.com>
2021-07-13 12:16:22 +0600

    updated

--- Python_Basic/Week1/String_operations.py
+++ Python_Basic/Week1/String_operations.py
@@ -2,3 +2,33 @@
 str1 = 'Sakir'
 str2 = 'Ahamed'
 print(str1 + " " + str2)
+
+# String Repetition
+repetition = "Bangladesh "
+print(repetition * 3)
+
+# Some important function
+# Join
+elements = ['first', 'second', 'third']
+join_elements = ','.join(elements)
+print(join_elements)
+print('Find out different between these two prints')
+print(elements)
+
+# Replace
+str3 = 'Sakir Ahamed'
+new_str3 = str3.replace('Sakir Ahamed', 'Ahamed Sakir')
+print(f'You name is changed {str3} to {new_str3}')
+
+# Upper case
+print('This is bangladesh'.upper())
+
+# Lower case
+print('THis is Lowercase'.lower())
+
+# Split
+together_string = "My name is sakir Ahamaled. I love to do code. I live in Bangladesh"
+individual_words = together_string.split(" ")
+print(individual_words)
+
+
---
+++ Python_Basic/Week1/type_conversion.py
@@ -0,0 +1,34 @@
+# String to integer
+str1 = "123456"
+str1_to_int = int(str1)
+print('This is an integer:', str1_to_int)
+
+# Float to Integer
+float_num = 6.2
+float_to_int = int(float_num)
+print(float_to_int)
+
+# Integer to float
+int_num = 6
+int_to_float = float(int_num)
+print('This is a float number:', int_to_float)
+
+# Critical conversion between string_float_number to integer
+string_value = '1245.256'
+string_to_float = float(string_value)
+float_to_int = int(string_to_float)
+print('string float number conversion:', float_to_int)
+
+"""N.B: int('1245.256') creates an error"""
+
+# String conversion
+num = 777
+num_to_string = str(num)
+print('This is string number:',num_to_string)
+
+# Sometimes we need to str conversion in print function
+num = 25
+print('This is ' + str(num) + ' number')
+
+
+
