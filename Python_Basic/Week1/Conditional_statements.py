69958345bc514d0c36e0b5d5ed4550408953de40
Ahamed-Sakir <87180676+Ahamed-Sakir@users.noreply.github.com>
2021-07-13 14:45:38 +0600

    updated

---
+++ Python_Basic/Week1/Conditional_statements.py
@@ -0,0 +1,37 @@
+# If condition
+x = 5
+y = 8
+if x < y:
+    print('Condition is true')
+
+# if-else condition
+if x > y:
+    print('Condition is true')
+else:
+    print('Condition is false')
+
+# elif
+x = 9
+y = 9
+if x > y:
+    print('x is greater than y')
+elif x == y:
+    print('x is equal to y')
+else:
+    print('x is smaller than y')
+
+"""How to execute conditional statement with minimal code"""
+x, y = 10, 8
+result = 'x is greater than y' if (x > y) else 'x is smaller or equal to y'
+print(result)
+
+# Ternary operator
+a = 50
+b = 100 if (a <= 100 and a < 100) else 200
+print(b)
+
+# While loop
+i = 1
+while i < 10:
+    print(i)
+    i += 2
--- Python_Basic/Week1/README.md
+++ Python_Basic/Week1/README.md
@@ -2,4 +2,5 @@
 
 ### String Operation
 * Check out more about [Split](https://docs.python.org/3/library/stdtypes.html#str.split) operation.
-* Check out more about [String_Method](https://docs.python.org/3/library/stdtypes.html#string-methods).
+* Check out more about [String method](https://docs.python.org/3/library/stdtypes.html#string-methods).
+* Check out more about [Conditinal statements](https://www.guru99.com/if-loop-python-conditional-structures.html).
