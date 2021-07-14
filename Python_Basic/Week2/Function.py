652dc9539615baa176f3978526ef0d7921743f3b
Ahamed-Sakir <87180676+Ahamed-Sakir@users.noreply.github.com>
2021-07-14 12:58:36 +0600

    Updated

--- Python_Basic/Week2/Function.py
+++ Python_Basic/Week2/Function.py
@@ -1,4 +1,35 @@
 def func():
     print('You are calling me')
 
+
 func()
+
+
+# Function with argument
+def func_double(x):
+    return x * 2
+
+
+return_value = func_double(6)
+print(return_value)
+
+
+# Multiple arguments
+def total_sum(*values):
+    sum = 0
+    for num in values:
+        sum += num
+
+    return sum
+
+
+print(total_sum(44, 6, 10, 70))
+
+
+# Double **
+def my_dic(**values):
+    dic = values
+    return dic
+
+
+print(my_dic(a=1, b=2, c=3))
