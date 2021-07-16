74da3d519c0f0515ed9d03bcb1ea36f2725b0b46
Ahamed-Sakir <87180676+Ahamed-Sakir@users.noreply.github.com>
2021-07-16 12:31:15 +0600

    Updated

---
+++ Python_Basic/Week2/OOP.py
@@ -0,0 +1,29 @@
+class Shirt:
+    def __init__(self, shirt_color, shirt_size, shirt_style, shirt_price):
+        self.color = shirt_color
+        self.size = shirt_size
+        self.style = shirt_style
+        self.price = shirt_price
+
+    def change_price(self, new_price):
+        self.price = new_price
+
+    def discount(self, discount):
+        return self.price * (1 - discount)
+
+
+#Shirt('Black', 'L', 'Formal', 1500)
+shirt_one = Shirt('Red', 'M', 'Casual', 200)
+print(shirt_one.price)
+
+shirt_gallery = []
+shirt_one = Shirt('Black', 'L', 'Casual', 1500)
+shirt_two = Shirt('White', 'XL', 'Formal', 2000)
+shirt_three = Shirt('Purple', 'L', 'Short shirt', 1200)
+
+shirt_gallery.append(shirt_one)
+shirt_gallery.append(shirt_two)
+shirt_gallery.append(shirt_three)
+
+for i in range(len(shirt_gallery)):
+    print(shirt_gallery[i].color, shirt_gallery[i].price)
